e33d4c1663cd0b9b9e38b95e11a128d2e8e8df15
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2024-03-13 15:20:17 +0700

    server working

--- lib/server/src/main/java/ru/nsu/fit/smolyakov/sobakacloud/server/ComputingServlet.java
+++ lib/server/src/main/java/ru/nsu/fit/smolyakov/sobakacloud/server/ComputingServlet.java
@@ -6,19 +6,21 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import ru.nsu.fit.smolyakov.sobakacloud.server.dto.ArgDto;
 import ru.nsu.fit.smolyakov.sobakacloud.server.dto.RequestDto;
+import ru.nsu.fit.smolyakov.sobakacloud.server.dto.ResponseDto;
 import ru.nsu.fit.smolyakov.sobakacloud.server.executor.BytesClassLoader;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
 
 public class ComputingServlet extends DefaultServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         var parts = req.getParts();
         if (parts.size() != 2) {
-            resp.sendError(400, "only classFile and requestInfo allowed and required");
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "only classFile and requestInfo allowed and required");
             return;
         }
 
@@ -30,7 +32,7 @@ public class ComputingServlet extends DefaultServlet {
                 case "classFile" -> classBytes = part.getInputStream().readAllBytes();
                 case "requestInfo" -> requestDtoBytes = part.getInputStream().readAllBytes();
                 default -> {
-                    resp.sendError(400, "only classFile and requestInfo allowed and required");
+                    resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "only classFile and requestInfo allowed and required");
                     return;
                 }
             }
@@ -50,7 +52,7 @@ public class ComputingServlet extends DefaultServlet {
         try {
             method = clazz.getMethod(requestDto.getEntryMethodName(), argTypes);
         } catch (NoSuchMethodException e) {
-            resp.sendError(400, "no public method called " + requestDto.getEntryMethodName());
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "no public method called " + requestDto.getEntryMethodName());
             return;
         }
 
@@ -60,7 +62,7 @@ public class ComputingServlet extends DefaultServlet {
             .toArray(Object[]::new);
 
         if (!Modifier.isStatic(method.getModifiers())) {
-            resp.sendError(400, "method named " + requestDto.getEntryMethodName() + " is not static");
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "method named " + requestDto.getEntryMethodName() + " is not static");
             return;
         }
 
@@ -69,13 +71,16 @@ public class ComputingServlet extends DefaultServlet {
         try {
             res = method.invoke(null, args);
         } catch (IllegalAccessException e) {
-            resp.sendError(400, "illegal access");
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "illegal access");
             return;
         } catch (InvocationTargetException e) {
-            resp.sendError(400, "invocation target exception");
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "invocation target exception");
             return;
         }
 
         ArgDto argDto = ArgDto.argDtoFromValue(res, method.getReturnType());
+        ResponseDto responseDto = new ResponseDto(argDto);
+        byte[] responseDtoBytes = responseDto.serialize();
+        resp.getOutputStream().write(responseDtoBytes);
     }
 }
