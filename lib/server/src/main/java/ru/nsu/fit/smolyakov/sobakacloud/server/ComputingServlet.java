d885168805d4556f22ebe4566070604ac29fbe20
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2024-03-13 13:21:37 +0700

    server part almost done

--- lib/server/src/main/java/ru/nsu/fit/smolyakov/sobakacloud/server/ComputingServlet.java
+++ lib/server/src/main/java/ru/nsu/fit/smolyakov/sobakacloud/server/ComputingServlet.java
@@ -4,32 +4,80 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.servlet.DefaultServlet;
+import ru.nsu.fit.smolyakov.sobakacloud.server.dto.ArgDto;
+import ru.nsu.fit.smolyakov.sobakacloud.server.dto.RequestDto;
+import ru.nsu.fit.smolyakov.sobakacloud.server.executor.BytesClassLoader;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.Arrays;
+import java.util.List;
 
 public class ComputingServlet extends DefaultServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        for (var part : req.getParts()) {
-            InputStream stream = part.getInputStream();
-            byte[] bytes = stream.readAllBytes();
+        var parts = req.getParts();
+        if (parts.size() != 2) {
+            resp.sendError(400, "only classFile and requestInfo allowed and required");
+            return;
+        }
 
-//            System.out.println("name is " + part.getName());
-//            System.out.println(Arrays.toString(bytes));
+        byte[] requestDtoBytes = null;
+        byte[] classBytes = null;
+
+        for (var part : req.getParts()) {
             switch (part.getName()) {
-                case "classFile" -> {
-                    System.err.println("classFile");
-                }
-                case "requestInfo" -> {
-                    System.err.println("requestInfo");
-                }
+                case "classFile" -> classBytes = part.getInputStream().readAllBytes();
+                case "requestInfo" -> requestDtoBytes = part.getInputStream().readAllBytes();
                 default -> {
-                    // error
-                    System.err.println("error");
+                    resp.sendError(400, "only classFile and requestInfo allowed and required");
+                    return;
                 }
             }
         }
+
+        var bytesClassLoader = new BytesClassLoader();
+
+        RequestDto requestDto = RequestDto.deserialize(requestDtoBytes);
+        Class<?> clazz = bytesClassLoader.loadClassFromBytes(classBytes);
+
+        Class<?>[] argTypes = requestDto.getArgs()
+            .stream()
+            .map(ArgDto::getArgValueClass)
+            .toArray(Class<?>[]::new);
+
+        Method method = null;
+        try {
+            method = clazz.getMethod(requestDto.getEntryMethodName(), argTypes);
+        } catch (NoSuchMethodException e) {
+            resp.sendError(400, "no public method called " + requestDto.getEntryMethodName());
+            return;
+        }
+
+        Object[] args = requestDto.getArgs()
+            .stream()
+            .map(ArgDto::getArgValueAsObject)
+            .toArray(Object[]::new);
+
+        if (!Modifier.isStatic(method.getModifiers())) {
+            resp.sendError(400, "method named " + requestDto.getEntryMethodName() + " is not static");
+            return;
+        }
+
+        Object res = null;
+//        method.getReturnType();
+        try {
+            res = method.invoke(null, args);
+        } catch (IllegalAccessException e) {
+            resp.sendError(400, "illegal access");
+            return;
+        } catch (InvocationTargetException e) {
+            resp.sendError(400, "invocation target exception");
+            return;
+        }
+
+        ArgDto argDto = ArgDto.argDtoFromValue(res, method.getReturnType());
     }
 }
--- lib/server/src/main/java/ru/nsu/fit/smolyakov/sobakacloud/server/dto/RequestDto.java
+++ lib/server/src/main/java/ru/nsu/fit/smolyakov/sobakacloud/server/dto/RequestDto.java
@@ -1,12 +1,40 @@
 package ru.nsu.fit.smolyakov.sobakacloud.server.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class RequestDto {
+    public static RequestDto deserialize(InputStream stream)  {
+        try {
+            return JacksonMappingSingleton.INSTANCE.getMapper().readValue(stream, RequestDto.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);  // todo custom
+        }
+    }
+
+    public static RequestDto deserialize(byte[] bytes)  {
+        try {
+            return JacksonMappingSingleton.INSTANCE.getMapper().readValue(bytes, RequestDto.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);  // todo custom
+        }
+    }
+
+    public byte[] serialize() {
+        try {
+            return JacksonMappingSingleton.INSTANCE.getWriter().writeValueAsBytes(this);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e); //todo custom
+        }
+    }
+
     @JsonProperty("entryMethodName")
     private String entryMethodName;
+
     @JsonProperty("args")
     private List<ArgDto> args;
 
